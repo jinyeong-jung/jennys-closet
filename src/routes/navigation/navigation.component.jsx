7f0cf360a00157d9b27ae9260748392b26cfbe34
Jinyeong Jung <jinyeong32@gmail.com>
2022-07-27 13:20:01 +0900

    adding redux, react-redux, redux-logger and settting up user & categories related redux code

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,7 +4,6 @@ import { Outlet, Link } from "react-router-dom";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
@@ -16,9 +15,11 @@ import {
   NavLinksContainer,
   NavLink,
 } from "./navigation.styles";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
